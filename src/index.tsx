aa679283dd2d975eb88b9d1f5ee0eb6bc5f45ec6
PainScript <35187547+Trendymen@users.noreply.github.com>
2024-08-05 14:30:56 +0800

    feat: commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App/App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import "antd/dist/antd.min.css";
 import "./styles/index.scss";
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
